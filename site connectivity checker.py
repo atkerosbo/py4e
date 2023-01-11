afa1ef9858bf53268c7c24edcb9a1d7dc0868bc3
atkerosbo <atkerosbo@yahoo.com>
2023-01-11 21:52:20 +0100

    1.0

---
+++ site connectivity checker.py
@@ -0,0 +1,14 @@
+#site connectivity checker
+import urllib.request as urllib
+
+def main(url):
+    print("Checking connectivity")
+
+    response = urllib.urlopen(url)
+    print("Connected to", url, "succesfully")
+    print("the response code was: " , response.getcode())
+
+print("This is a site connectivity checker program")
+input_url = input("Input the URL of the site: ")
+
+main(input_url)
